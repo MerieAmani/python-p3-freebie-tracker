f7d30355bd3434b71417c51d9ed630a6d0567f6b
Matthew <meriematthew01@gmail.com>
2025-05-30 14:30:39 +0300

    adds correction

--- lib/seed.py
+++ lib/seed.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -9,25 +7,21 @@ engine = create_engine('sqlite:///freebies.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Clear existing data (optional, for repeatable seeding)
 session.query(Freebie).delete()
 session.query(Company).delete()
 session.query(Dev).delete()
 session.commit()
 
-# Create companies
 c1 = Company(name="Google", founding_year=1998)
 c2 = Company(name="Microsoft", founding_year=1975)
 session.add_all([c1, c2])
 session.commit()
 
-# Create devs
 d1 = Dev(name="Alice")
 d2 = Dev(name="Bob")
 session.add_all([d1, d2])
 session.commit()
 
-# Create freebies
 f1 = Freebie(item_name="T-shirt", value=20, company=c1, dev=d1)
 f2 = Freebie(item_name="Sticker", value=2, company=c1, dev=d2)
 f3 = Freebie(item_name="Mug", value=10, company=c2, dev=d1)
